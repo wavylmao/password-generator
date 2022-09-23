b73bde3ddaee4b77f8a388881694865da4dbc33a
ddev187 <yawnhenley@gmail.com>
2022-09-22 21:50:26 -0400

    Add frontend components

--- src/App.js
+++ src/App.js
@@ -1,11 +1,61 @@
-import "../public/styles.css";
+import { useState } from "react";
+import "./styles.scss";
 
 export default function App() {
+
+  const [rangeValue, setRangeValue] = useState("1");
+
+  function handleRangeChange(){
+    setRangeValue(document.getElementById("rangeSelector").value);
+  }
+
   return (
     <div className="App">
       <div className="wrapper">
         <h1 className="heading">Password Generator</h1>
-        <input className="password-output" type="text" readOnly />
+        <div className="output-container">
+          <input
+            value=""
+            placeholder="P4$5W0rD!"
+            className="password-output"
+            type="text"
+            readOnly
+          />
+        </div>
+        <div className="param-container">
+          <h3>Character Length</h3>
+          <h1 className="length-counter">{rangeValue}</h1>
+          <div className="range-container">
+            <input
+              onChange={handleRangeChange}
+              id="rangeSelector"
+              class="range-input"
+              type="range"
+              min="1"
+              max="20"
+              value={rangeValue}
+            />
+          </div>
+          <div className="checkbox-container">
+            <input className="checkbox" type="checkbox" />
+            <h3 className="checkbox-params">Include Uppercase Letters</h3>
+            <input className="checkbox" type="checkbox" />
+            <h3 className="checkbox-params">Include Lowercase Letters</h3>
+            <input className="checkbox" type="checkbox" />
+            <h3 className="checkbox-params">Include Numbers</h3>
+            <input className="checkbox" type="checkbox" />
+            <h3 className="checkbox-params">Include Symbols</h3>
+            <input
+              value=""
+              placeholder="STRENGTH"
+              className="strength-output"
+              type="text"
+              readOnly
+            />
+            <br />
+            <button className="generate-button">Generate</button>
+          </div>
+        </div>
       </div>
     </div>
   );
